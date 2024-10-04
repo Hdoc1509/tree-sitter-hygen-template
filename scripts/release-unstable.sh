20aeefa20473c1165056dec62cec9a016e7e0fd7
hdoc <hector.ochoa.dev@gmail.com>
2024-10-03 20:57:57 -0500

    chore: split release script
    resulting scripts:
    - release.sh -> to use after stable version (v1.0.0) has been released
    - release-unstable.sh -> until stable version has been released

--- scripts/release-unstable.sh
+++ scripts/release-unstable.sh
@@ -22,24 +22,38 @@ else
   previous_version=$(get_version_from_package_json)
 fi
 
+# changes since last release
+breaking_changes_count=0
+unstable_message_regex='^\w+(\(.*\))?:!'
+git_tags_count=$(git tag | wc --lines)
+
+get_last_tag() { git describe --tags --abbrev=0; }
+
+if [[ $git_tags_count -gt 0 ]]; then
+  breaking_changes_count=$(
+    git log "$(get_last_tag)"..HEAD --oneline |
+      grep --count --extended-regexp "$unstable_message_regex"
+  )
+fi
+
 # release and update releated files
 npx changeset version
 
 new_version=$(get_version_from_changelog)
-major_change_count=$(head --line=5 CHANGELOG.md | grep --count "Major")
 
 # NOTE: uncomment to test
-# major_change_count=1
+breaking_changes_count=1
 
-if [[ $major_change_count -gt 0 ]]; then
+if [[ $breaking_changes_count -gt 0 ]]; then
   breaking_changes_message=$(
     sed \
-      -e "s/{{ compatible_semver }}/minor/" \
-      -e "s/{{ previous_version }}/\`^$previous_version\` or \`~$previous_version\`/" \
+      -e "s/{{ compatible_semver }}/patch/" \
+      -e "s/{{ previous_version }}/\`~$previous_version\`/" \
       docs/breaking-change-message.txt
   )
   echo
-  echo "[RELEASE]: Major change detected, generating breaking change message..."
+  echo "[RELEASE]: Unstable version release!"
+  echo "[RELEASE]: Breaking changes detected, generating breaking change message..."
   sed -i "4 a$breaking_changes_message\n" CHANGELOG.md
 fi
 
