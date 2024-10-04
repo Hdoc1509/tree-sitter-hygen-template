72b22de4647d8d5732918989d1cae6d7c1ed5b43
hdoc <hector.ochoa.dev@gmail.com>
2024-10-04 11:19:35 -0500

    refactor(scripts): implement utils for releases

--- scripts/release-unstable.sh
+++ scripts/release-unstable.sh
@@ -1,71 +1,47 @@
-# package versioning files
-pyproject_line=8
-cargo_line=4
-previous_version="0.0.0"
+REPO_ROOT=$(git rev-parse --show-toplevel)
+export REPO_ROOT
 
-get_version_from_changelog() {
-  head --lines=3 CHANGELOG.md | tail --lines=1 | awk '{ print $2 }'
-}
-
-update_version_in() {
-  target_file=$1
-  target_line=$2
-
-  # TODO: at least on digit per version part
-  # sed -i "$target_line s/[0-9]\+\.[0-9]\+\.[0-9]\+/$new_version/" "$target_file"
-  # NOTE: $new_version is updated dynamically
-  sed -i "$target_line s/[0-9]\.[0-9]\.[0-9]/$new_version/" "$target_file"
-}
+source "$REPO_ROOT"/scripts/utils.sh
 
 # INITIAL RELEASE
 if ! [[ -f CHANGELOG.md ]]; then
   echo "[RELEASE]: Initial release!"
-  echo "[RELEASE]: Skipping breaking changes check..."
+  echo "[RELEASE]: Breaking changes check will be skipped!"
+  echo
+
   npx changeset version
 
   # changelog is now generated
-  new_version=$(get_version_from_changelog)
-
-  sed -i "5 s/Minor Changes/Initial release/" CHANGELOG.md
-
-  update_version_in pyproject.toml "$pyproject_line"
-  update_version_in Cargo.toml "$cargo_line"
+  update_new_version "$(get_version_from_changelog)"
+  set_changelog_initial_release_message
+  update_package_files_version
 
   exit 0
 fi
 
 # NORMAL RELEASE
-get_last_tag() { git describe --tags --abbrev=0; }
-
 previous_version=$(get_version_from_changelog)
 
 # changes since last release
+unstable_message_regex='^\w+(\(.*\))?:!'
 breaking_changes_count=$(
   git log "$(get_last_tag)"..HEAD --oneline |
     grep --count --extended-regexp "$unstable_message_regex"
 )
-unstable_message_regex='^\w+(\(.*\))?:!'
+
+echo "[RELEASE]: Unstable version release!"
+echo
 
 # genrate changelog and update version
 npx changeset version
 
-new_version=$(get_version_from_changelog)
+update_new_version "$(get_version_from_changelog)"
 
 # NOTE: uncomment to test
 # breaking_changes_count=1
 
 if [[ $breaking_changes_count -gt 0 ]]; then
-  breaking_changes_message=$(
-    sed \
-      -e "s/{{ compatible_semver }}/patch/" \
-      -e "s/{{ previous_version }}/\`~$previous_version\`/" \
-      docs/breaking-change-message.txt
-  )
-  echo
-  echo "[RELEASE]: Unstable version release!"
-  echo "[RELEASE]: Breaking changes detected, generating breaking change message..."
-  sed -i "4 a$breaking_changes_message\n" CHANGELOG.md
+  set_changelog_breaking_changes_message "patch" "$previous_version"
 fi
 
-update_version_in pyproject.toml "$pyproject_line"
-update_version_in Cargo.toml "$cargo_line"
+update_package_files_version
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,61 +1,21 @@
-# package versioning files
-pyproject_line=8
-cargo_line=4
+REPO_ROOT=$(git rev-parse --show-toplevel)
+export REPO_ROOT
 
-get_version_from_changelog() {
-  head --lines=3 CHANGELOG.md | tail --lines=1 | awk '{ print $2 }'
-}
+source "$REPO_ROOT"/scripts/utils.sh
 
-update_version_in() {
-  target_file=$1
-  target_line=$2
-
-  # TODO: at least on digit per version part
-  # sed -i "$target_line s/[0-9]\+\.[0-9]\+\.[0-9]\+/$new_version/" "$target_file"
-  # NOTE: $new_version is updated dynamically
-  sed -i "$target_line s/[0-9]\.[0-9]\.[0-9]/$new_version/" "$target_file"
-}
-
-# INITIAL RELEASE
-if ! [[ -f CHANGELOG.md ]]; then
-  echo "[RELEASE]: Initial release!"
-  echo "[RELEASE]: Skipping breaking changes check..."
-  npx changeset version
-
-  # changelog is now generated
-  new_version=$(get_version_from_changelog)
-
-  sed -i "5 s/Minor Changes/Initial release/" CHANGELOG.md
-
-  update_version_in pyproject.toml "$pyproject_line"
-  update_version_in Cargo.toml "$cargo_line"
-
-  exit 0
-fi
-
-# NORMAL RELEASE
 previous_version=$(get_version_from_changelog)
 
 # genrate changelog and update version
 npx changeset version
 
-new_version=$(get_version_from_changelog)
+update_new_version "$(get_version_from_changelog)"
 major_change_count=$(head --line=5 CHANGELOG.md | grep --count "Major")
 
 # NOTE: uncomment to test
 # major_change_count=1
 
 if [[ $major_change_count -gt 0 ]]; then
-  breaking_changes_message=$(
-    sed \
-      -e "s/{{ compatible_semver }}/minor/" \
-      -e "s/{{ previous_version }}/\`^$previous_version\` or \`~$previous_version\`/" \
-      docs/breaking-change-message.txt
-  )
-  echo
-  echo "[RELEASE]: Major change detected, generating breaking change message..."
-  sed -i "4 a$breaking_changes_message\n" CHANGELOG.md
+  set_changelog_breaking_changes_message "minor" "$previous_version"
 fi
 
-update_version_in pyproject.toml "$pyproject_line"
-update_version_in Cargo.toml "$cargo_line"
+update_package_files_version
