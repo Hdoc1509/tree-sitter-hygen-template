02c7f229ff56d129a3239cbd1ac16c9339b0d4a7
Hdoc1509 <Hdoc1509@users.noreply.github.com>
2024-11-27 01:31:41 +0000

    deploy: 88e5c7e69528e8621e4c10f7a6961db57699b915

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -2,7 +2,7 @@
 
 typedef struct TSLanguage TSLanguage;
 
-extern "C" TSLanguage *tree_sitter_hygen-template();
+extern "C" TSLanguage *tree_sitter_hygen_template();
 
 // "tree-sitter", "language" hashed with BLAKE2
 const napi_type_tag LANGUAGE_TYPE_TAG = {
@@ -10,11 +10,11 @@ const napi_type_tag LANGUAGE_TYPE_TAG = {
 };
 
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports["name"] = Napi::String::New(env, "hygen-template");
-    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_hygen-template());
+    exports["name"] = Napi::String::New(env, "hygen_template");
+    auto language = Napi::External<TSLanguage>::New(env, tree_sitter_hygen_template());
     language.TypeTag(&LANGUAGE_TYPE_TAG);
     exports["language"] = language;
     return exports;
 }
 
-NODE_API_MODULE(tree_sitter_hygen-template_binding, Init)
+NODE_API_MODULE(tree_sitter_hygen_template_binding, Init)
