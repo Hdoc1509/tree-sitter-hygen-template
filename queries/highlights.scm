65d08d8d03bf62268e9134ba702fdc8cddf1346d
Hdoc1509 <Hdoc1509@users.noreply.github.com>
2025-07-04 03:40:31 +0000

    deploy: f94d50cb4dbce375a921c49e17f50fa43ec74a1b

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,21 +1,30 @@
-; inherits: embedded_template
+(frontmatter
+  (metadata
+    ((key) @property
+      (#any-of? @property
+        "to" "from" "force" "unless_exists" "inject" "after" "skip_if" "sh"))))
 
-(template
-  (frontmatter
-    (metadata
-      ((key) @property
-        (#any-of? @property
-          "to"
-          "from"
-          "force"
-          "unless_exists"
-          "inject"
-          "after"
-          "skip_if"
-          "sh")))))
+(frontmatter
+  (metadata
+    (value
+      (string_value) @string)))
 
-(template
-  (frontmatter
-    (metadata
-      (value
-        (string_value) @string))))
+(frontmatter
+  "---" @punctuation.delimiter)
+
+(frontmatter
+  (metadata
+    ":" @punctuation.delimiter))
+
+(comment_directive) @comment
+
+[
+  "<%#"
+  "<%"
+  "<%="
+  "<%_"
+  "<%-"
+  "%>"
+  "-%>"
+  "_%>"
+] @keyword
