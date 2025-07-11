daed137d6181c6cfb4b458625eaf4faeeaaa18a4
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-11 17:45:14 -0500

    feat(scripts): implement `normal_release()` util

--- scripts/release-normal.sh
+++ scripts/release-normal.sh
@@ -1,6 +1,24 @@
 release_normal() {
   local previous_version=$1
+  local changelog_file=$2
+  local breaking_changes_count
 
   echo -e "[RELEASE]: Normal release!\n"
-  echo -e "[RELEASE]: previous version: $previous_version"
+
+  trigger_release
+  update_new_version
+
+  breaking_changes_count=$(
+    sed --quiet '5p' "$changelog_file" | grep --count "Major"
+  )
+
+  # NOTE: uncomment to test
+  # major_change_count=1
+
+  if [[ $breaking_changes_count -gt 0 ]]; then
+    set_changelog_breaking_changes_message "minor" "$previous_version"
+  fi
+
+  update_package_files_version "$previous_version"
+  reminder_message
 }
