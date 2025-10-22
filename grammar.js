9691698def95ef9d2b2e8e872920bc3a00ba56b2
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-10-22 11:39:14 -0500

    refactor: remove unuseful comment

--- grammar.js
+++ grammar.js
@@ -7,9 +7,6 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-// NOTE: AVOID USAGE OF `*` (wildcard) INSIDE `repeat()` or `repeat1()` functions.
-// IT CAN LEAD TO INFINITE LOOPS!!!
-
 const embedded_template = require("tree-sitter-embedded-template/grammar");
 
 module.exports = grammar(embedded_template, {
