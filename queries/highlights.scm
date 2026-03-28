8c08be0f1371f3a2f41684c6d62b8ce6577277f9
Hdoc1509 <hector.ochoa.dev@gmail.com>
2026-03-28 11:47:26 -0500

    refactor(queries): simplify highlight queries

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,21 +1,16 @@
-(frontmatter
-  (metadata
-    ((key) @property
-      (#any-of? @property
-        "to" "from" "force" "unless_exists" "inject" "after" "skip_if" "sh" "before" "prepend"
-        "append" "at_line"))))
+; follow STANDARD_CAPTURE_NAMES:
+; https://github.com/tree-sitter/tree-sitter/blob/master/crates/highlight/src/highlight.rs
+((key) @property
+  (#any-of? @property
+    "to" "from" "force" "unless_exists" "inject" "after" "skip_if" "sh" "before" "prepend" "append"
+    "at_line"))
 
-(frontmatter
-  (metadata
-    (value
-      (string_value) @string)))
+(string_value) @string
 
-(frontmatter
-  "---" @punctuation.delimiter)
-
-(frontmatter
-  (metadata
-    ":" @punctuation.delimiter))
+[
+  "---"
+  ":"
+] @punctuation.delimiter
 
 (comment_directive) @comment
 
@@ -30,15 +25,9 @@
   "_%>"
 ] @keyword
 
-(frontmatter
-  (metadata
-    (value
-      [
-        (true)
-        (false)
-      ] @boolean)))
+[
+  (true)
+  (false)
+] @boolean
 
-(frontmatter
-  (metadata
-    (value
-      (number) @number)))
+(number) @number
